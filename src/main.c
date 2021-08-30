ee822a97b038626a8e2aea5c6095352dd7a8755d
yoann foreau <yoann.foreau@ito.am>
2021-08-30 14:47:47 +0200

    Duplicate argv so that exec_name is argv[0]
    This is a bit heavy. It would be useful to add an exec_name
    or prog_name member to the optdata structure for ft_getopt.
    It would allow to set a custom name for the program in case
    it is different from argv[0]. TODO

--- src/main.c
+++ src/main.c
@@ -6,33 +6,43 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 04:30:15 by yforeau           #+#    #+#             */
-/*   Updated: 2021/08/30 14:22:48 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/08/30 14:46:15 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-int	main(int argc, char **argv)
+void	get_options(t_pingcfg *cfg, int argc, char **argv)
 {
 	int			opt;
-	t_pingcfg	cfg;
 	t_optdata	optd;
+	char		**args;
 
-	(void)argc;
-	ft_bzero((void *)&cfg, sizeof(t_pingcfg));
 	ft_bzero((void *)&optd, sizeof(t_optdata));
-	cfg.exec_name = ft_exec_name(*argv);
-	ft_exitmsg((char *)cfg.exec_name);
 	init_getopt(&optd, FT_PING_OPT, NULL, NULL);
-	while ((opt = ft_getopt(argc, argv, &optd)) == 'v')
-		cfg.verbose = 1;
-	if (cfg.verbose)
-		ft_printf("%s: VERBOSE MODE ON\n", cfg.exec_name);
+	args = ft_memalloc((argc + 1) * sizeof(char *));
+	ft_memcpy((void *)args, (void *)argv, argc * sizeof(char *));
+	*args = (char *)cfg->exec_name;
+	while ((opt = ft_getopt(argc, args, &optd)) == 'v')
+		cfg->verbose = 1;
+	if (cfg->verbose)
+		ft_printf("%s: VERBOSE MODE ON\n", cfg->exec_name);
 	if (opt != -1)
 	{
-		ft_printf(FT_PING_HELP, cfg.exec_name);
+		ft_printf(FT_PING_HELP, cfg->exec_name);
 		ft_exit(NULL, opt != 'h');
 	}
+	ft_memdel((void **)&args);
+}
+
+int	main(int argc, char **argv)
+{
+	t_pingcfg	cfg;
+
+	ft_bzero((void *)&cfg, sizeof(t_pingcfg));
+	cfg.exec_name = ft_exec_name(*argv);
+	ft_exitmsg((char *)cfg.exec_name);
+	get_options(&cfg, argc, argv);
 	ft_printf("This is %s!\n", cfg.exec_name);
 	ft_exit(NULL, EXIT_SUCCESS);
 	return (EXIT_SUCCESS);
