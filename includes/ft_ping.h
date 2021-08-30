5f203f354e6eb10c396bbb4864a3b6c6e53a4d10
yoann foreau <yoann.foreau@ito.am>
2021-08-30 18:19:22 +0200

    Use allowed global variable for the configuration

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 04:34:28 by yforeau           #+#    #+#             */
-/*   Updated: 2021/08/30 14:59:03 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/08/30 18:10:27 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,25 @@
 # define FT_PING_H
 
 # include "libft.h"
+# include <netdb.h>
 
 /*
-** Global ping configuration structure
+** Ping configuration structure
 **
 ** exec_name: name of the ft_ping executable
 */
-typedef struct	s_pingcfg
+typedef struct		s_pingcfg
 {
-	const char	*exec_name;
-	const char	*destination;
-	int			verbose;
-}				t_pingcfg;
+	const char		*exec_name;
+	const char		*destination;
+	int				verbose;
+	struct addrinfo	*destinfo;
+}					t_pingcfg;
+
+/*
+** Global instance of the structure
+*/
+extern t_pingcfg	*g_cfg;
 
 #define	FT_PING_OPT		"vh"
 #define	FT_PING_HELP	"Usage:\n\t%s [options] <destination>\n"\
--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 04:30:15 by yforeau           #+#    #+#             */
-/*   Updated: 2021/08/30 15:03:01 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/08/30 18:17:35 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-char	*get_options(t_pingcfg *cfg, int argc, char **argv)
+static char	*get_options(int argc, char **argv)
 {
 	int			opt;
 	t_optdata	optd;
@@ -22,35 +22,64 @@ char	*get_options(t_pingcfg *cfg, int argc, char **argv)
 	init_getopt(&optd, FT_PING_OPT, NULL, NULL);
 	args = ft_memalloc((argc + 1) * sizeof(char *));
 	ft_memcpy((void *)args, (void *)argv, argc * sizeof(char *));
-	*args = (char *)cfg->exec_name;
+	*args = (char *)g_cfg->exec_name;
 	while ((opt = ft_getopt(argc, args, &optd)) == 'v')
-		cfg->verbose = 1;
-	if (cfg->verbose)
-		ft_printf("%s: VERBOSE MODE ON\n", cfg->exec_name);
+		g_cfg->verbose = 1;
+	if (g_cfg->verbose)
+		ft_printf("%s: VERBOSE MODE ON\n", g_cfg->exec_name);
 	if (opt != -1)
 	{
-		ft_printf(FT_PING_HELP, cfg->exec_name);
+		ft_printf(FT_PING_HELP, g_cfg->exec_name);
 		ft_exit(NULL, opt != 'h');
 	}
 	ft_memdel((void **)&args);
 	return (argv[optd.optind]);
 }
 
+static struct addrinfo	*get_destinfo(void)
+{
+	int				ret;
+	char			*err;
+	struct addrinfo	*res;
+	struct addrinfo	hints;
+
+	ft_bzero((void *)&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_ICMP;
+	if (!(ret = getaddrinfo(g_cfg->destination, NULL, NULL, &res)))
+		ft_printf("getaddrinfo: HUGE SUUCCESSSS\n");
+	ft_printf("getaddrinfo return: %d\n", ret);
+	if (ret)
+	{
+		ft_asprintf(&err, "%s: %s", g_cfg->destination, gai_strerror(ret));
+		ft_exit(err, EXIT_FAILURE);
+	}
+	return (res);
+}
+
+static void	ping_cleanup(void)
+{
+	if (g_cfg->destinfo)
+		freeaddrinfo(g_cfg->destinfo);
+}
+
+t_pingcfg	*g_cfg = NULL;
+
 int	main(int argc, char **argv)
 {
-	t_pingcfg	cfg;
+	t_pingcfg	cfg = { 0 };
 
-	ft_bzero((void *)&cfg, sizeof(t_pingcfg));
-	cfg.exec_name = ft_exec_name(*argv);
-	ft_exitmsg((char *)cfg.exec_name);
-	if (!(cfg.destination = get_options(&cfg, argc, argv)))
-	{
-		ft_dprintf(2, "%s: usage error: Destination address required\n",
-			cfg.exec_name);
-		ft_exit(NULL, EXIT_FAILURE);
-	}
-	ft_printf("This is %s!\n", cfg.exec_name);
-	ft_printf("destination is: %s\n", cfg.destination);
+	g_cfg = &cfg;
+	ft_atexit(ping_cleanup);
+	g_cfg->exec_name = ft_exec_name(*argv);
+	ft_exitmsg((char *)g_cfg->exec_name);
+	if (!(g_cfg->destination = get_options(argc, argv)))
+		ft_exit("usage error: Destination address required", EXIT_FAILURE);
+	//TODO: check ICMP socket permission with getpid and getuid
+	g_cfg->destinfo = get_destinfo();
+	ft_printf("This is %s!\n", g_cfg->exec_name);
+	ft_printf("destination is: %s\n", g_cfg->destination);
 	ft_exit(NULL, EXIT_SUCCESS);
 	return (EXIT_SUCCESS);
 }
