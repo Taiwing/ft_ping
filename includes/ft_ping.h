d11204481172a599acc99d1f519384874f7ea0c3
yoann foreau <yoann.foreau@ito.am>
2021-08-30 12:57:47 +0200

    Get name of executable and create config structure

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 04:34:28 by yforeau           #+#    #+#             */
-/*   Updated: 2021/08/23 04:37:39 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/08/30 12:54:33 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define FT_PING_H
 
 # include "libft.h"
+
+/*
+** Global ping configuration structure
+**
+** exec_name: name of the ft_ping executable
+*/
+typedef struct	s_pingcfg
+{
+	const char	*exec_name;
+}				t_pingcfg;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 04:30:15 by yforeau           #+#    #+#             */
-/*   Updated: 2021/08/23 04:35:03 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/08/30 12:54:59 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
+	t_pingcfg	cfg;
 
-	ft_exitmsg(*argv);
-	ft_printf("This is %s!\n", *argv);
+	(void)argc;
+	ft_bzero((void *)&cfg, sizeof(t_pingcfg));
+	cfg.exec_name = ft_exec_name(*argv);
+	ft_exitmsg((char *)cfg.exec_name);
+	ft_printf("This is %s!\n", cfg.exec_name);
 	ft_exit(NULL, EXIT_SUCCESS);
 	return (EXIT_SUCCESS);
 }
